d548ac8968d6f52e277777d371cc45a9339954cd
Techwolf78 <mrajaypawar207@gmail.com>
2024-06-17 00:23:21 +0530

    updated

--- databases/db.js
+++ databases/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      "mongodb+srv://newkingdom5878:exM9HFsg7GbBE3iF@cluster0.vpvoowb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log(`Connected To MongoDB Database ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error in MongoDB: ${error}`);
